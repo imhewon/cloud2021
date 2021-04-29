4381218c81e9e27eb377f8e08762bdd59fac69fc
Hewang23 <belay23@163.com>
2021-04-29 21:39:04 +0800

    添加了Euerka、Ribbon、OpenFeign以及Hystrix部分

--- cloud-consumer-order80/src/main/java/com/hewon/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/hewon/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.hewon.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
+//    @LoadBalanced 使用自己手写的负载均衡算法
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- cloud-provider-payment8001/src/main/java/com/hewon/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/hewon/springcloud/controller/PaymentController.java
@@ -4,25 +4,31 @@ import com.hewon.springcloud.entities.CommonResult;
 import com.hewon.springcloud.entities.Payment;
 import com.hewon.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-
-@RestController
+import java.util.List;
 @Slf4j
+@RestController
 public class PaymentController {
     @Resource
     private PaymentService paymentService;
+    @Resource
+    private DiscoveryClient discoveryClient;
+    @Value("${server.port}")
+    private String serverPort;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment)
     {
         int result = paymentService.create(payment);
-        log.info("*****插入结果："+result);
 
         if(result > 0)
         {
-            return new CommonResult(200,"插入数据库成功,",result);
+            return new CommonResult(200,"插入数据库成功,serverPort="+serverPort,result);
         }else{
             return new CommonResult(444,"插入数据库失败",null);
         }
@@ -35,10 +41,28 @@ public class PaymentController {
         int i = 10 / 2;
         if(payment != null)
         {
-            return new CommonResult(200,"查询成功,",payment);
+            return new CommonResult(200,"查询成功,serverPort=" + serverPort,payment);
         }else{
             return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
         }
     }
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery()
+    {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*****element: "+element);
+        }
 
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
+    }
+    @GetMapping(value = "/payment/lb")
+    public String getPaymentLB() {
+        return serverPort;//返回服务接口
+    }
 }
